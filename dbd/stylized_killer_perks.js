71394cf33170ffa8d91f749aae1ecd2d1ffa3e72
cazwacki <cazwacki@vt.edu>
2022-07-20 19:15:51 -0400

    Stylized addons for Billy, Onryo, Freddy, Myers, Legion, Huntress, Pig, Ghost Face

--- dbd/stylized_killer_perks.js
+++ dbd/stylized_killer_perks.js
@@ -1,27 +1,27 @@
 var stylized_killer_perks = {
     "Aura-Reading": {
         "perks": ["A Nurse's Calling", "Barbecue & Chilli", "Bitter Murmur", "Blood Warden", "Call of Brine", "Deadlock", "Dead Man's Switch", "Deerstalker", "Discordance", "Eruption", "Franklin's Demise", "Gearhead", "Grim Embrace", "Hangman's Trick", "Hex: Retribution", "Hex: Undying", "I'm All Ears", "Lethal Pursuer", "Lightborn", "Nemesis", "Rancor", "Scourge Hook: Gift of Pain", "Scourge Hook: Floods of Rage", "Surveillance", "Territorial Imperative", "Thrilling Tremors", "Zanshin Tactics"],
-        "addons": ["Tattoo's Middle Finger", "Cheap Gin Bottle", "Cigar Box", "Bottle of Chloroform", "Oily Coil", "Vibrant Obituary", "Oil Paints", "Still Life Crow", "Pussy Willow Catkins", "Willow Wreath", "Compound Twenty-One", "Victor's Soldier", "Light Chassis", "\"Restraint\" - Class II", "\"Restraint\" - Class III", , "\"Restraint\" - Carter's Notes", "Iridescent King", "\"Order\" - Carter's Notes", "\"Order\" - Class II", "\"Order\" - Carter's Notes", "\"Order\" - Class I"],
+        "addons": ["Tattoo's Middle Finger", "Cheap Gin Bottle", "Cigar Box", "Bottle of Chloroform", "Oily Coil", "Vibrant Obituary", "Oil Paints", "Still Life Crow", "Pussy Willow Catkins", "Willow Wreath", "Compound Twenty-One", "Victor's Soldier", "Light Chassis", "\"Restraint\" - Class II", "\"Restraint\" - Class III", , "\"Restraint\" - Carter's Notes", "Iridescent King", "\"Order\" - Carter's Notes", "\"Order\" - Class II", "\"Order\" - Carter's Notes", "\"Order\" - Class I", "Mother's Comb", "Remote Control", "Prototype Claws", "Green Dress", "Paint Thinner", "Blue Dress", "Reflective Fragment", "Glass Fragment", "Mirror Shard", "Vanity Mirror", "Scratched Mirror", "Stylish Sunglasses", "Stab Wounds Study", "Fuming Mix Tape", "Glowing Concoction", "Jigsaw's Sketch", "Amanda's Secret", "Amanda's Letter", "Olsen's Address Book", "Marked Map", "Outdoor Security Camera"],
         "description": "gather information"
     },
     "Anti-Gens": {
         "perks": ["Brutal Strength", "Call of Brine", "Corrupt Intervention", "Deadlock", "Dead Man's Switch", "Discordance", "Dragon's Grip", "Dying Light", "Eruption", "Grim Embrace", "Hex: Huntress Lullaby", "Hex: Pentimento", "Hex: Ruin", "Oppression", "Overcharge", "Pop Goes the Weasel", "Scourge Hook: Pain Resonance", "Jolt", "Merciless Storm", "Surveillance", "Thanatophobia", "Thrilling Tremors", "Tinkerer", "Unnerving Presence"],
-        "addons": ["Fingerless Parade Gloves", "Thick Cork Stopper", "Sticky Soda Bottle", "Flask of Bleach", "Bottle of Chloroform", "Cheap Gin Bottle"],
+        "addons": ["Fingerless Parade Gloves", "Thick Cork Stopper", "Sticky Soda Bottle", "Flask of Bleach", "Bottle of Chloroform", "Cheap Gin Bottle", "Wool Shirt", "Outdoor Rope", "Green Dress", "Blue Dress", "Frank's Mix Tape", "Fuming Mix Tape", "Driver's License"],
         "description": "slow down gen progression"
     },
     "Anti-Healing": {
         "perks": ["A Nurse's Calling", "Coulrophobia", "Deathbound", "Forced Penance", "Hex: Huntress Lullaby", "Hex: Pentimento", "Scourge Hook: Gift of Pain", "Sloppy Butcher", "Unnerving Presence"],
-        "addons": ["Redhead's Pinky Finger", "Sulphuric Acid Vial", "Cheap Gin Bottle", "Rusted Jaws", "Lengthened Jaws", "Wax Brick", "Fastening Tools", "Thorny Nest", "Severed Hands", "Weighty Rattle", "Rusted Chains", "Grisly Chains", "Iridescent Queen"],
+        "addons": ["Redhead's Pinky Finger", "Sulphuric Acid Vial", "Cheap Gin Bottle", "Rusted Jaws", "Lengthened Jaws", "Wax Brick", "Fastening Tools", "Thorny Nest", "Severed Hands", "Weighty Rattle", "Rusted Chains", "Grisly Chains", "Iridescent Queen", "Ring Drawing", "Wool Shirt", "Green Dress", "Jump Rope", "Blue Dress"],
         "description": "prevent or discourage healing"
     },
     "Anti-Items": {
         "perks": ["Franklin's Demise", "Hoarder", "Lightborn", "Overwhelming Presence"],
-        "addons": ["Spinning Top", "Begrimed Chains"],
+        "addons": ["Spinning Top", "Begrimed Chains", "Stolen Sketch Book"],
         "description": "punish the use of items"
     },
     "Anti-Looping": {
         "perks": ["Bamboozle", "Beast of Prey", "Blood Echo", "Brutal Strength", "Coup de Grâce", "Claustrophobia", "Enduring", "Hex: Blood Favour", "Hex: Crowd Control", "I'm All Ears", "Fearmonger", "Spirit Fury", "Zanshin Tactics"],
-        "addons": ["Starling Feather", "Sticky Soda Bottle", "Spirit of Hartshorn", "Smelly Inner Soles", "Flask of Bleach", "Garish Make-up Kit", "Secondary Coil", "Fastening Tools", "Tension Spring", "Matias' Baby Shoes", "Severed Tongue", "Silver Bell", "Untitled Agony", "Thick Tar", "O Grief, O Lover", "Summoning Stone", "Compound Thirty-Three", "Primer Bulb", "Chilli", "\"Order\" - Carter's Notes", "\"Order\" - Class II", "\"Order\" - Carter's Notes", "\"Order\" - Class I"],
+        "addons": ["Starling Feather", "Sticky Soda Bottle", "Spirit of Hartshorn", "Smelly Inner Soles", "Flask of Bleach", "Garish Make-up Kit", "Secondary Coil", "Fastening Tools", "Tension Spring", "Matias' Baby Shoes", "Severed Tongue", "Silver Bell", "Untitled Agony", "Thick Tar", "O Grief, O Lover", "Summoning Stone", "Compound Thirty-Three", "Primer Bulb", "Chilli", "\"Order\" - Carter's Notes", "\"Order\" - Class II", "\"Order\" - Carter's Notes", "\"Order\" - Class I", "Heavy Clutch", "Dad's Boots", "Mother's Helpers", "Spiked Boots", "LoPro Chains", "Telephone", "Garden Rake", "Prototype Claws", "Paint Thinner", "Iridescent Button", "Drop-Leg Knife Sheath"],
         "description": "end chases more quickly"
     },
     "Basic Attacking": {
@@ -31,7 +31,7 @@ var stylized_killer_perks = {
     },
     "BP Farming": {
         "perks": ["Barbecue & Chilli", "Beast of Prey", "Distressing", "Hex: Thrill of the Hunt"],
-        "addons": ["Party Bottle", "Scarred Hand", "Placebo Tablet", "Speed Limiter"],
+        "addons": ["Party Bottle", "Scarred Hand", "Placebo Tablet", "Speed Limiter", "Videotape Copy", "Kid's Drawing", "Vanity Mirror", "Scratched Mirror", "Never-Sleep Pills"],
         "description": "get more bloodpoints per match"
     },
     "Carrying": {
@@ -41,17 +41,17 @@ var stylized_killer_perks = {
     },
     "Confusing": {
         "perks": ["Dark Devotion", "Knock Out", "Lightborn", "Claustrophobia"],
-        "addons": ["VHS Porn", "Kerosene Can", "Bear Oil", "Tar Bottle", "Tension Spring", "Bloody Coil", "Iridescent Stone", "Charcoal Stick", "Darkest Ink", "Silver Bell", "Disfigured Ear", "Grandma's Heart", "Rusty Shackles", "Scarred Hand", "Soul Chemical", "Alchemist's Ring", "Summoning Stone", "Baby Teeth", "Drop of Perfume", "Homemade Muffler", "Iridescent Flesh", "\"Order\" - Carter's Notes", "\"Order\" - Class I", "\"Order\" - Carter's Notes", "\"Order\" - Class II", "\"Order\" - Carter's Notes", "\"Order\" - Carter's Notes", "\"Calm\" - Class I", "\"Discipline\" - Class II", "\"Calm\" - Class II", "\"Discipline\" - Class III", "\"Discipline\" - Carter's Notes", "\"Calm\" - Carter's Notes"],
+        "addons": ["VHS Porn", "Kerosene Can", "Bear Oil", "Tar Bottle", "Tension Spring", "Bloody Coil", "Iridescent Stone", "Charcoal Stick", "Darkest Ink", "Silver Bell", "Disfigured Ear", "Grandma's Heart", "Rusty Shackles", "Scarred Hand", "Soul Chemical", "Alchemist's Ring", "Summoning Stone", "Baby Teeth", "Drop of Perfume", "Homemade Muffler", "Iridescent Flesh", "\"Order\" - Carter's Notes", "\"Order\" - Class I", "\"Order\" - Carter's Notes", "\"Order\" - Class II", "\"Order\" - Carter's Notes", "\"Order\" - Carter's Notes", "\"Calm\" - Class I", "\"Discipline\" - Class II", "\"Calm\" - Class II", "\"Discipline\" - Class III", "\"Discipline\" - Carter's Notes", "\"Calm\" - Carter's Notes", "Tuned Carburettor", "VCR", "Pill Bottle", "Class Photo"],
         "description": "throw off survivors"
     },
     "End-Gaming": {
         "perks": ["Blood Warden", "Hex: No One Escapes Death", "No Way Out", "Remember Me"],
-        "addons": [],
+        "addons": ["Black Box", "Tombstone Piece", "Judith's Tombstone"],
         "description": "make a comeback in the endgame"
     },
     "Exposing": {
         "perks": ["Dragon's Grip", "Hex: Devour Hope", "Hex: Haunted Ground", "Hex: No One Escapes Death", "Hex: Thrill of the Hunt", "Hex: Undying", "Iron Maiden", "Make Your Choice", "Rancor", "Starstruck"],
-        "addons": ["Redhead's Pinky Finger", "Garden of Rot", "Iridescent Blight Tag", "Iridescent Pendant"],
+        "addons": ["Redhead's Pinky Finger", "Garden of Rot", "Iridescent Blight Tag", "Iridescent Pendant", "Blond Hair", "Hair Brush", "Hair Bow", "Lock of Hair", "Fragrant Tuft of Hair", "Iridescent Head", "Cheap Cologne", "\"Philly\""],
         "description": "expose survivors"
     },
     "Hexing": {
@@ -66,37 +66,37 @@ var stylized_killer_perks = {
     },
     "Obssessive": {
         "perks": ["Dark Devotion", "Dead Man's Switch", "Dying Light", "Furtive Chase", "Grim Embrace", "Nemesis", "Play with Your Food", "Rancor", "Save the Best for Last"],
-        "addons": [],
+        "addons": ["Judith's Journal"],
         "description": "interact with the Obsession"
     },
     "Scream-Inducing": {
         "perks": ["Deathbound", "Dragon's Grip", "Eruption", "Infectious Fright", "Make Your Choice", "Scourge Hook: Pain Resonance"],
-        "addons": ["Cheap Gin Bottle", "Bottle of Chloroform", "Sticky Soda Bottle", "Honing Stone", "Dried Cicada", "Bloodied Mud", "Iridescent Queen", "High Stimulus Electrode", "\"Calm\" - Class II", "\"Calm\" - Carter's Notes"],
+        "addons": ["Cheap Gin Bottle", "Bottle of Chloroform", "Sticky Soda Bottle", "Honing Stone", "Dried Cicada", "Bloodied Mud", "Iridescent Queen", "High Stimulus Electrode", "\"Calm\" - Class II", "\"Calm\" - Carter's Notes", "Distorted Photo"],
         "description": "make the survivors scream"
     },
     "Speedy": {
         "perks": ["Agitation", "Bamboozle", "Brutal Strength", "Coup de Grâce", "Fire Up", "Hex: Devour Hope", "Hex: No One Escapes Death", "Iron Maiden", "Play with Your Food", "Save the Best for Last", "Unrelenting"],
-        "addons": ["Solvent Jug", "Smelly Inner Soles", "Garish Make-up Kit", "Trapper Bag", "Trapper Gloves", "Makeshift Wrap", "Festering Carrion", "Automatic Drawing", "Severed Tongue", "Swamp Orchid Necklet", "Cypress Necklet", "Rope Necklet", "Foxglove", "Canker Thorn", "Adrenaline Vial", "Toy Sword", "Tiny Fingernail", "Bloody Black Hood", "Stale Biscuit", "Madeleine's Scarf", "Forest Stew", "Homemade Muffler", "Primer Bulb", "Spark Plug", "Chainsaw File", "\"Discipline\" - Carter's Notes", "\"Order\" - Carter's Notes", "\"Discipline\" - Class III", "\"Discipline\" - Class II", "\"Order\" - Class II", "\"Order\" - Class I"],
+        "addons": ["Solvent Jug", "Smelly Inner Soles", "Garish Make-up Kit", "Trapper Bag", "Trapper Gloves", "Makeshift Wrap", "Festering Carrion", "Automatic Drawing", "Severed Tongue", "Swamp Orchid Necklet", "Cypress Necklet", "Rope Necklet", "Foxglove", "Canker Thorn", "Adrenaline Vial", "Toy Sword", "Tiny Fingernail", "Bloody Black Hood", "Stale Biscuit", "Madeleine's Scarf", "Forest Stew", "Homemade Muffler", "Primer Bulb", "Spark Plug", "Chainsaw File", "\"Discipline\" - Carter's Notes", "\"Order\" - Carter's Notes", "\"Discipline\" - Class III", "\"Discipline\" - Class II", "\"Order\" - Class II", "\"Order\" - Class I", "Steel Toe Boots", "Punctured Muffler", "Death Engravings", "Doom Engravings", "Low Kickback Chains", "Cabin Sign", "Yoichi's Fishing Net", "Well Stone", "Bloody Fingernails", "Nancy's Sketch", "Nancy's Masterpiece", "Tacky Earrings", "Memorial Flower", "Jewelry", "Jewelry Box", "J.Myers Memorial", "Scratched Ruler", "Mischief List", "Friendship Bracelet", "Mural Sketch", "Frank's Mix Tape", "BFFs", "Yellowed Cloth", "Shiny Pin", "Oak Haft", "Manna Grass Braid", "Rose Root", "Flower Babushka", "Deerskin Gloves", "Soldier's Puttee", "Shattered Syringe", "John's Medical File", "Combat Straps", "Workshop Grease", "Last Will", "Jigsaw's Annotated Plan", "Bag of Gears", "Tampered Timer", "Crate of Gears", "\"Philly\"", "Walleye's Matchbook", "Headline Cut-Outs", "Leather Knife Sheath", "Drop-Leg Knife Sheath"],
         "description": "speed up some of your actions"
     },
     "Status-Inflicting": {
         "perks": ["Blood Echo", "Deathbound", "Eruption", "Forced Penance", "Hex: Retribution", "Hex: The Third Seal", "Hex: Plaything", "Hysteria", "Fearmonger", "Nemesis", "Scourge Hook: Gift of Pain", "Sloppy Butcher"],
-        "addons": ["Redhead's Pinky Finger", "Flask of Bleach", "Starling Feather", "Lengthened Jaws", "Serrated Jaws", "Rusted Jaws", "Garden of Rot", "Severed Hands", "Darkest Ink", "O Grief, O Lover", "Silver Bell", "Thorny Nest", "Waterlogged Shoe", "Drop of Perfume", "Rusted Needle", "Weighty Rattle", "Shop Lubricant", "Grisly Chains", "Rusted Chains"],
+        "addons": ["Redhead's Pinky Finger", "Flask of Bleach", "Starling Feather", "Lengthened Jaws", "Serrated Jaws", "Rusted Jaws", "Garden of Rot", "Severed Hands", "Darkest Ink", "O Grief, O Lover", "Silver Bell", "Thorny Nest", "Waterlogged Shoe", "Drop of Perfume", "Rusted Needle", "Weighty Rattle", "Shop Lubricant", "Grisly Chains", "Rusted Chains", "Sea-Soaked Cloth", "Ring Drawing", "Rickety Pinwheel", "Telephone", "Tape Editing Deck", "Cat Block", "Unicorn Block", "\"Z\" Block", "Etched Ruler", "Defaced Smiley Pin", "The Legion Pin", "Joey's Mix Tape", "Amanita Toxin", "Weighted Head", "Venomous Concoction", "Rusty Head", "Begrimed Head", "Interlocking Razor", "Utility Blades", "Face Mask", "Slow-Release Toxin", "Rusty Attachments", "Rules Set No.2", "Telephoto Lens", "Olsen's Journal", "Victim's Detailed Routine", "Night Vision Monocular"],
         "description": "inflict status effects on survivors"
     },
     "Stealthy": {
         "perks": ["Beast of Prey", "Dark Devotion", "Furtive Chase", "Hex: The Third Seal", "Hex: Plaything", "Insidious", "Monitor & Abuse", "Tinkerer", "Trail of Torment"],
-        "addons": ["Bear Oil", "Silver Bell", "Iridescent Feather", "Grandma's Heart", "Rusty Shackles", "Vigo's Journal", "Silencing Cloth", "Cat's Eye", "Shop Lubricant", "\"Calm\" - Class I", "\"Calm\" - Class II", "\"Calm\" - Carter's Notes"],
+        "addons": ["Bear Oil", "Silver Bell", "Iridescent Feather", "Grandma's Heart", "Rusty Shackles", "Vigo's Journal", "Silencing Cloth", "Cat's Eye", "Shop Lubricant", "\"Calm\" - Class I", "\"Calm\" - Class II", "\"Calm\" - Carter's Notes", "Off-Brand Motor Oil", "Big Buckle", "Leafy Mash", "Pighouse Gloves", "Apex Muffler", "Iridescent Brick", "Old Newspaper", "Mother's Mirror", "Reiko's Watch", "Clump of Hair", "Well Water", "Rickety Pinwheel", "Pill Bottle", "Wooden Fox", "Cinch Straps", "Olsen's Wallet", "\"Ghost Face Caught on Tape\"", "Knife Belt Clip"],
         "description": "catch survivors by surprise"
     },
     "Terrifying": {
         "perks": ["Agitation", "Coulrophobia", "Distressing", "Infectious Fright", "Merciless Storm", "Overwhelming Presence", "Starstruck", "Unnerving Presence"],
-        "addons": ["Cracked Turtle Egg", "Soul Chemical", "\"Calm\" - Class I", "\"Calm\" - Class II", "\"Calm\" - Carter's Notes"],
+        "addons": ["Cracked Turtle Egg", "Soul Chemical", "\"Calm\" - Class I", "\"Calm\" - Class II", "\"Calm\" - Carter's Notes", "Dead Rabbit", "Susie's Mix Tape"],
         "description": "take advantage of your terror radius"
     },
     "Tracking": {
         "perks": ["Barbecue & Chilli", "Blood Echo", "Bloodhound", "Deerstalker", "Discordance", "Iron Maiden", "I'm All Ears", "Lethal Pursuer", "No Way Out", "Predator", "Scourge Hook: Floods of Rage", "Shadowborn", "Sloppy Butcher", "Spies from the Shadows", "Stridor", "Whispers"],
-        "addons": ["Flask of Bleach", "Cigar Box", "Cheap Gin Bottle", "Serrated Jaws", "Matias' Baby Shoes", "Thorny Nest", "Velvet Fabric", "Bloodied Water", "Bloodied Mud", "Compound Twenty-One", "Soul Chemical", "Soured Milk", "Madeleine's Glove", "Victor's Soldier", "\"Restraint\" - Class II", "\"Restraint\" - Class III", "\"Restraint\" - Carter's Notes"],
+        "addons": ["Flask of Bleach", "Cigar Box", "Cheap Gin Bottle", "Serrated Jaws", "Matias' Baby Shoes", "Thorny Nest", "Velvet Fabric", "Bloodied Water", "Bloodied Mud", "Compound Twenty-One", "Soul Chemical", "Soured Milk", "Madeleine's Glove", "Victor's Soldier", "\"Restraint\" - Class II", "\"Restraint\" - Class III", "\"Restraint\" - Carter's Notes", "Cat Block", "Jump Rope", "Swing Chains", "\"Z\" Block", "Reflective Fragment", "Glass Fragment", "Mirror Shard", "Vanity Mirror", "Scratched Mirror", "Coarse Stone", "Glowing Concoction"],
         "description": "track survivors"
     }
 }
