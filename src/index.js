ee24c268b464b2e12335b3e933cf516c4428f8ac
cazwacki <cazwacki@vt.edu>
2022-08-13 11:08:11 -0400

    SEO Improvement

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route }
+import { HashRouter as Router, Routes, Route }
     from 'react-router-dom';
 import Header from './components/header'
 import AboutMe from './pages/about'
