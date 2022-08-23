424f834ae18cb9291995b4f979a3466f1cdb9b9b
Charles Zawacki <cazwacki@vt.edu>
2022-08-23 19:22:46 +0000

    Remove commented code from postCreate

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -4,15 +4,7 @@
 npm install -g npm
 npm config set cache /workspaces/cazwacki.github.io/cache
 
-# allow for deployments to GitHub via npm run deploy
-# sudo chown -R 1000:1000 /root/.config
-# mkdir -p /root/.config/git
-# echo """
-# [user]
-# 	email = cazwacki@vt.edu
-# 	name = Charles Zawacki
-# """ > /root/.config/git/attributes
-
+# configure git info for this repo
 cd /workspaces/cazwacki.github.io
 git config user.email "cazwacki@vt.edu"
 git config user.name "Charles Zawacki"
